c0f94694923845a5915931811c2acd11e8a56f44
Theodore-PKU <xieyutong@pku.edu.cn>
2021-11-16 15:28:15 +0800

    Fix a bug. Original fp16_util is imported from external package. Now we add fp_util file and can import it from interior part.

--- sr_var/ddpm/unet.py
+++ sr_var/ddpm/unet.py
@@ -7,7 +7,7 @@ import torch as th
 import torch.nn as nn
 import torch.nn.functional as F
 
-from sr_utils.fp16_util import convert_module_to_f16, convert_module_to_f32
+from .fp16_util import convert_module_to_f16, convert_module_to_f32
 from .nn import (
     checkpoint,
     conv_nd,
